49863aaa23c107d8837494bebbdb222d32c9e15f
Stephen Woods <stephen.woods@salesforce.com>
2020-11-09 10:57:03 -0800

    Restructure to never write to input buffer

--- src/main.rs
+++ src/main.rs
@@ -20,21 +20,29 @@ fn getLuma(pixel:&image::Rgba<u8> ) -> u8 {
 fn getAdjustedPixel(old_pixel:image::Rgba<u8>, adjustment:i8) -> u8 {
     
     let mut new_pixel = Rgba([old_pixel[0], old_pixel[1], old_pixel[2], 255]);
-    let newluma = getLuma(&new_pixel);
-    // // 
-    return newluma;
+    let luma = getLuma(&new_pixel) as i32;
+    let adjusted:i32 = luma + adjustment as i32;
+    let mut newluma = adjusted;
+    if newluma < 0 {
+        newluma = 255;
+    } else if newluma > 255 {
+        newluma = 255;
+    }
+    return newluma as u8;
 }
 
 fn main() {
     // Use the open function to load an image from a Path.
     // `open` returns a `DynamicImage` on success.
-    let img = image::open("IMG_0076.jpg").unwrap();
-    let dims = img.dimensions();
+    let mut read = image::open("IMG_0076.jpg").unwrap();
+    let dims = read.dimensions();
+    let img = read.into_rgba();
     
     let mut img2: RgbaImage = ImageBuffer::new(dims.0, dims.1);
-    
-    for pixel in img.pixels() {
-        let my_pix = pixel.2;
+    img2.put_pixel(0,0, *img.get_pixel(0,0));
+    for mut pixel in img.enumerate_pixels() {
+        // println!("Pix: {:?}", pixel);
+        let mut my_pix = img2.get_pixel(pixel.0, pixel.1);
         let oldluma =  getLuma(&my_pix);
         let mut newluma:u8 = 0;
         if oldluma > 127 {
@@ -53,8 +61,12 @@ fn main() {
         if (pixel.0 < dims.0-1) {
             next_pixel = img.get_pixel(pixel.0 +1, pixel.1);
             let quant_error_transformed:f32 = quant_error as f32 * 7.0/16.0;
-            adjval = getAdjustedPixel(next_pixel, quant_error as i8); // cast to u8 same as floor and faster
+            adjval = getAdjustedPixel(*next_pixel, quant_error as i8); // cast to u8 same as floor and faster
+            // next_pixel
+            let fpix = Rgba([adjval, adjval, adjval, 255]);
+            img2.put_pixel(pixel.0+1, pixel.1, fpix);
         }
+
 
         /*
         pixel[x + 1][y    ] := pixel[x + 1][y    ] + quant_error × 7 / 16
@@ -63,20 +75,7 @@ fn main() {
         pixel[x + 1][y + 1] := pixel[x + 1][y + 1] + quant_error × 1 / 16
         */
 
-        // let avg:u32 = (old_pixel / 3).into();
-        // println!("old pix {:?}", avg);
-        // if pixel.0 > 2 && pixel.1 > 2 {
-        //     // let mypix = pixel.2;
-        //     // let mut rng = rand::thread_rng();
-        //     // let rnum = rng.gen_range(0,255);
-        //     let mut val = 0;
-        //     if pixel.2.0[0] > 207 {
-        //         val = 255;
-        //     }
-        //     let pix = Rgba([val, val, val, 255]);
-        //     img2.put_pixel(pixel.0, pixel.1, pix);
 
-        // }
               
     }
     img2.save("test.png").unwrap();
