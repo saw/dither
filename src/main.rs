39021d2fabb6048bdc33475135ce967b3920418b
Stephen Woods <stephen.woods@salesforce.com>
2020-11-09 11:10:03 -0800

    floyd steinberg

--- src/main.rs
+++ src/main.rs
@@ -73,11 +73,32 @@ fn main() {
             index_x = pixel.0 as i32 - 1;
             index_y = pixel.1 as i32 + 1;
             next_pixel = img.get_pixel(index_x as u32, index_y as u32);
-            let quant_error_transformed:f32 = quant_error as f32 * 7.0/16.0;
+            let quant_error_transformed:f32 = quant_error as f32 * 3.0/16.0;
             adjval = getAdjustedPixel(*next_pixel, quant_error as i8); // cast to u8 same as floor and faster
             let fpix = Rgba([adjval, adjval, adjval, 255]);
             img2.put_pixel(index_x as u32, index_y as u32, fpix);
         }
+
+        if pixel.1 < dims.1 -1 {
+            index_x = pixel.0 as i32;
+            index_y = pixel.1 as i32 + 1;
+            next_pixel = img.get_pixel(index_x as u32, index_y as u32);
+            let quant_error_transformed:f32 = quant_error as f32 * 5.0/16.0;
+            adjval = getAdjustedPixel(*next_pixel, quant_error as i8); // cast to u8 same as floor and faster
+            let fpix = Rgba([adjval, adjval, adjval, 255]);
+            img2.put_pixel(index_x as u32, index_y as u32, fpix);
+        }
+
+        if pixel.0 < dims.0-1 && pixel.1 < dims.1 -1 {
+            index_x = pixel.0 as i32 + 1;
+            index_y = pixel.1 as i32 + 1;
+            next_pixel = img.get_pixel(index_x as u32, index_y as u32);
+            let quant_error_transformed:f32 = quant_error as f32 * 1.0/16.0;
+            adjval = getAdjustedPixel(*next_pixel, quant_error as i8); // cast to u8 same as floor and faster
+            let fpix = Rgba([adjval, adjval, adjval, 255]);
+            img2.put_pixel(index_x as u32, index_y as u32, fpix);
+        }
+
 
 
         /*
